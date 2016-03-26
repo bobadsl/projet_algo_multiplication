cbf359ba4f5b68c07db41f8c6bd1fc1b83bb73ac
Bouchaib KHAFIF <marmotton@iMac-de-Bouchaib.local>
2016-03-26 11:30:02 +0100

    Deuxieme version de mult ==> additions successives

--- fct.c
+++ fct.c
@@ -1,9 +1,19 @@
 /**
  * Fonction retournant le resultat de la multiplication de a*b
+ * 
+ * Algorithme : addition successive
+ *
  * @param a (int ) : premier nombre
  * @param b (int)  : deuxieme nombre
  * @return (int) : résultat de la multiplication
  */
 int mult(int a,int b){
-	return a*b;
+	int i; // indice de la boucle
+	int res = 0; // resultat de la multiplication
+
+	// res = a + a + a + a + .... + a (b fois)
+	for (i=0; i<b;i++)
+		res+=a;	
+
+	return res;
 }
